f1ac3a41ec11006f8687fd6a10b6e4555223066f
leeyaz <leeya.zhang5976@gmail.com>
2025-02-11 14:26:52 -0800

    Update wordGuesserGame.js

---
+++ wordGuesserGame.js
@@ -0,0 +1,6 @@
+
+
+//select mystery word from datasheet
+
+//ask to guess mystery word, give hints with each guess, eg: smth letters away, how many correct vowels
+//keep guessing until find correct word. each guess loses a point or smth.
