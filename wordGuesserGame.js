2bad18d10c20241bcf078717e65e8c4adad852aa
leeyaz <leeya.zhang5976@gmail.com>
2025-02-13 13:15:30 -0800

    tested node.js method of taking word from dataset in wordGuesserGame.js file

--- wordGuesserGame.js
+++ wordGuesserGame.js
@@ -1,8 +1,18 @@
 
 //select mystery word from datasheet
-function getword() {
-    
+function getword(index) {
+    const fs = require('fs');
+    fs.readFile("datasets/word_list_dataset", "utf8", (err, data) => {
+        if (err) {
+            var errmessage = "Error reading this file: " + err
+            return errmessage;
+        }
+        const lines = data.split("\n");
+        const item = lines[index];
+        return item;
+    });
 }
 
+console.log(getword(1));
 //ask to guess mystery word, give hints with each guess, eg: smth letters away, how many correct vowels
 //keep guessing until find correct word. each guess loses a point or smth.
