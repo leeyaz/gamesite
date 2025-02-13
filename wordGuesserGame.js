b9a6e799e12099e23e1551c927121244b09efa31
leeyaz <leeya.zhang5976@gmail.com>
2025-02-13 14:34:32 -0800

    can spit out word from dataset now

--- wordGuesserGame.js
+++ wordGuesserGame.js
@@ -1,21 +1,35 @@
 
 //select mystery word from datasheet
-const fs = require('fs').promises; 
-function getrow(index) {
-    return fs.readFile("datasets/word_list_dataset", "utf8")
-        .then(data => {
-            const lines = data.split("\n");
-            return lines[index];
-        })
-        .catch(err => {
-            console.error("Error reading this file:", err);
-            return null;
-        });
+const fs = require('fs');
+const readline = require('readline');
+
+function getWord(index) {
+    const fileStream = fs.createReadStream('datasets/word_list_dataset.txt');
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let currentIndex = 0;
+
+    rl.on('line', (line) => {
+        const parts = line.split(',').map(part => part.trim());
+        if (parseInt(parts[0]) === index) {
+            console.log(parts[1]); 
+            rl.close(); 
+        }
+        currentIndex++;
+    });
+
+    rl.on('close', () => {
+        if (currentIndex < index) {
+            console.log('Index out of bounds.');
+        }
+    });
 }
 
-getrow(1).then(item => {
-    console.log(item);
-});
+getWord(1);
 
 
 //ask to guess mystery word, give hints with each guess, eg: smth letters away, how many correct vowels
