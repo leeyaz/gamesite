bc67208c07e17e88cc75b02774ee97e9aa67a49a
leeyaz <leeya.zhang5976@gmail.com>
2025-02-13 12:46:45 -0800

    added template to index.html, created style.css, created barebone function for wordGuesserGame.js

--- wordGuesserGame.js
+++ wordGuesserGame.js
@@ -1,5 +1,8 @@
 
 //select mystery word from datasheet
+function getword() {
+    
+}
 
 //ask to guess mystery word, give hints with each guess, eg: smth letters away, how many correct vowels
 //keep guessing until find correct word. each guess loses a point or smth.
